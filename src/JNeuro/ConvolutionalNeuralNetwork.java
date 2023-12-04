cf9a82b13eaac5f120f7c6af5fe154882260dd9d
ZHANG Xinjie <xinjzhang8-c@my.cityu.edu.hk>
2023-12-04 18:17:21 +0800

    little changes

--- src/JNeuro/ConvolutionalNeuralNetwork.java
+++ src/JNeuro/ConvolutionalNeuralNetwork.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 public class ConvolutionalNeuralNetwork {
     private ArrayList<Layer> layers = new ArrayList<Layer>();
+    private String weightPath;
+    private boolean isStartNewly;
 
     private double[][][] forward(double[][][] input) {
         double[][][] result = input;
@@ -34,23 +36,27 @@ public class ConvolutionalNeuralNetwork {
         }
     }
 
-    public ConvolutionalNeuralNetwork() {
-        this.layers.add(new ConvolutionLayer(16, 28, 28, 3));
-        this.layers.add(new PoolingLayer()); // this result in 8*14*14
+    public ConvolutionalNeuralNetwork(boolean isStartNewly, String pathOfWeight) {
+        this.layers.add(new ConvolutionLayer(32, 28, 28, 3));
+        this.layers.add(new PoolingLayer());
         // this.layers.add(new SoftMaxLayer(8, 14, 14, 1, 14, 14));// hidden layer
         // this.layers.add(new SoftMaxLayer(1, 14, 14, 1, 14, 14));// hidden layer 2
-        this.layers.add(new SoftMaxLayer(16, 14, 14, 1, 1, 10));// output layer
+        this.layers.add(new SoftMaxLayer(32, 14, 14, 1, 1, 10));// output layer
+        this.weightPath = pathOfWeight;
+        this.isStartNewly = isStartNewly;
+        if (!isStartNewly) {
+            this.load(pathOfWeight);
+        }
     }
 
-    public void train(int trainningSize, ImageDataFrame trainningData, String path) throws IOException {
-        this.load(path);
+    public void train(int trainningSize, ImageDataFrame trainningData) throws IOException {
+        System.out.println("start training...");
         int trueLabel = 0;
         double loss = 0;
         double accuracy = 0;
         double totalAccuracy = 0.0;
-        double learnRate = 0.005;
+        double learnRate = isStartNewly ? 0.005 : 0.001;
         double[][][] softmaxResult3D;
-        double lastAccuracy = 0.0;
         for (int step = 1; step <= trainningSize; step++) {
             double[][][] pixels = trainningData.sampledFromLabelIncrementally(trueLabel);
             int correct_label = trueLabel;
@@ -62,28 +68,24 @@ public class ConvolutionalNeuralNetwork {
             double[][] gradient = MathUtil.zeros(1, 10);
             gradient[0][correct_label] = -1 / softmaxResult[0][correct_label];
             this.backPropagation(MathUtil.reshape(gradient, 1, 1, 10), learnRate);
-            if (step % 1000 == 0) {
+            learnRate *= 0.9999; // learning rate decay
+            if (step % 10000 == 0) {
                 System.out
-                        .print(" step: " + step + " loss: " + loss / 1000.0 + " accuracy: " + accuracy / 10.0 + "%");
-                if (lastAccuracy >= accuracy) {
-                    learnRate *= 0.99;
-                    System.out.println(" learn rate: " + learnRate);
-                } else {
-                    learnRate *= 1.005;
-                    System.out.println(" learn rate: " + learnRate);
-                }
+                        .println(" step: " + step + " loss: " + loss / 10000.0 + " accuracy: " + accuracy / 100.0 + "%"
+                                + " learnRate: " + learnRate);
                 loss = 0;
                 totalAccuracy += accuracy;
-                lastAccuracy = accuracy;
                 accuracy = 0;
             }
         }
         System.out.println("tranning average accuracy:- " + totalAccuracy / trainningSize * 100 + "%");
-        this.save(path);
+        this.save(this.weightPath);
+        isStartNewly = false;
+        System.out.println("training finished");
     }
 
-    public void test(int testSize, ImageDataFrame testData, String path) throws IOException {
-        this.load(path);
+    public void test(int testSize, ImageDataFrame testData) throws IOException {
+        System.out.println("start testing...");
         int trueLabel = 0;
         double totalAccuracy = 0.0;
         for (int step = 1; step <= testSize; step++) {
@@ -95,5 +97,6 @@ public class ConvolutionalNeuralNetwork {
             totalAccuracy += correct_label == MathUtil.argmax(softmaxResult) ? 1 : 0;
         }
         System.out.println("test average accuracy:- " + totalAccuracy / testSize * 100 + "%");
+        System.out.println("testing finished");
     }
 }
--- src/JNeuro/JNeuro.java
+++ src/JNeuro/JNeuro.java
@@ -6,19 +6,19 @@ import java.io.IOException;
 public class JNeuro {
 
     private static void train(int training_size, String pathOfReadInData, String pathOfWeight) throws IOException {
-        ConvolutionalNeuralNetwork cnn = new ConvolutionalNeuralNetwork();
+        ConvolutionalNeuralNetwork cnn = new ConvolutionalNeuralNetwork(false, pathOfWeight);
         ImageDataFrame trainningData = new ImageDataFrame(pathOfReadInData);
-        cnn.train(training_size, trainningData, pathOfWeight);
+        cnn.train(training_size, trainningData);
     }
 
     private static void test(int test_size, String pathOfReadInData, String pathOfWeight) throws IOException {
-        ConvolutionalNeuralNetwork cnn = new ConvolutionalNeuralNetwork();
+        ConvolutionalNeuralNetwork cnn = new ConvolutionalNeuralNetwork(false, pathOfWeight);
         ImageDataFrame testData = new ImageDataFrame(pathOfReadInData);
-        cnn.test(test_size, testData, pathOfWeight);
+        cnn.test(test_size, testData);
     }
 
     public static void main(String[] args) throws IOException {
-        train(3000, "data/training", "weight");
+        train(300000, "data/training", "weight");
         test(100, "data/training", "weight");
     }
 
